98daac1f6825ff6512da55ca9857373eea7f3a9d
Vedant Kahalekar <98043938+VedantKCSE@users.noreply.github.com>
2022-09-29 20:24:32 +0530

    Update Array_Op.c

--- Array_Op.c
+++ Array_Op.c
@@ -1,7 +1,15 @@
-// program
+/* 	Array Operatons
+	Name: Vedant Kahalekar
+	Roll No.: 08
+	Class: SY-A (CSe)
+	Batch: S1
+	Date: 29-09-2022
+*/
 #include <stdio.h>
 #include <stdlib.h>
-int a[100],n, i, value, position;
+#include <conio.h>
+
+int a[100], n, i, value, position;
 
 void create();
 void display();
@@ -9,116 +17,138 @@ void insert();
 void del();
 void search();
 
-int main()
+void main()
 {
     int choice = 1;
-    while (choice<=6)
+    clrscr();
+    create();
+    do
     {
-        printf("\n\n--------MENU-----------\n");
-        printf("1.CREATE\n");
-        printf("2.DISPLAY\n");
-        printf("3.INSERT\n");
-        printf("4.DELETE\n");
-        printf("5.SEARCH\n");
-        printf("6.EXIT\n");
-        printf("-----------------------");
-        printf("\nENTER YOUR CHOICE:\t");
-
-        scanf("%d", &choice);
-        
-        switch (choice)
-        {
-        case 1:
-            create();
-            break;
-        case 2:
-            display();
-            break;
-        case 3:
-            insert();
-            break;
-        case 4:
-            del();
-            break;
-        case 5:
-            search();
-        case 6:
-            exit(0);
-        default:
-            printf("\nInvalid choice:\n");
-            break;
-        }
-    }
-    return 0;
+	printf("\n\n*-------MENU-------*\n");
+	printf("1.DISPLAY\n");
+	printf("2.INSERT\n");
+	printf("3.DELETE\n");
+	printf("4.SEARCH\n");
+	printf("5.EXIT\n");
+	printf("*------------------*");
+	printf("\nEnter Your Choice:\t");
+	scanf("%d", &choice);
+	switch (choice)
+	{
+	case 1:
+	    if(n == 0)
+	    {
+		printf("No elements to display");
+	    }else{
+	    display();
+	    }
+	    break;
+	case 2:
+	    insert();
+	    display();
+	    break;
+	case 3:
+	    if(n == 0)
+	    {
+		printf("No elements to delete");
+	    }else{
+	    del();
+	    display();
+	    }
+	    break;
+	case 4:
+	    if(n == 0)
+	    {
+	    printf("Array is empty.");
+	    }else{
+	    search();
+	    }
+	    break;
+	case 5:
+	    exit(0);
+	    break;
+	default:
+	    printf("\nInvalid choice\n");
+	    break;
+	}
+    }while (choice <= 5);
+    getch();
 }
 
-// creatng array elements
+// Creating array elements
 void create()
 {
-    printf("\nEnter the size of the array elements:\t");
+    printf("\nEnter the size of array:\t");
     scanf("%d", &n);
-    printf("\nEnter the elements for the array:\n");
+    printf("\nEnter the elements of array:\n");
     for (i = 0; i < n; i++)
     {
-        scanf("%d", &a[i]);
+	scanf("%d", &a[i]);
     }
 }
 
-
-// displaying an array elements
+// displaying array elements
 void display()
 {
     int i;
-    printf("\nThe array elements are:\n");
+    printf("\nArray elements are:\n");
     for (i = 0; i < n; i++)
     {
-        printf("%d\t", a[i]);
+	printf("%d\t", a[i]);
     }
 }
 
-
-// inserting an element into an array
+// Inserting element into an array
 void insert()
 {
-    printf("\nEnter the position for the new element:\t");
+    printf("\nEnter the position for the new element:\t\n");
     scanf("%d", &position);
-    printf("\nEnter the element to be inserted :\t");
-    scanf("%d", &value);
-    for (i = n - 1; i >= position-1; i--)
+    if(position<=n)
     {
-        a[i + 1] = a[i];
+	printf("\nEnter the element to be inserted :\t");
+	scanf("%d", &value);
+	for (i = n - 1; i >= position - 1; i--)
+		{
+			a[i + 1] = a[i];
+		}
+	a[position - 1] = value;
+	n = n + 1;
+    }else
+    {
+	printf("Enter the correct position between 1 to %d\n",n);
     }
-    a[position-1] = value;
-    n = n + 1;
 }
 
-
-// deleting an array element
+// Deleting an array element
 void del()
 {
     printf("\nEnter the position of the element to be deleted:\t");
     scanf("%d", &position);
-    value = a[position-1];
-    for (i = position-1; i < n - 1; i++)
+    value = a[position - 1];
+    for (i = position - 1; i < n - 1; i++)
     {
-        a[i] = a[i + 1];
+	a[i] = a[i + 1];
     }
     n = n - 1;
-    printf("\nThe deleted element is =%d", value);
+    printf("\nThe deleted element is = %d", value);
 }
 
-// Searching element in an array 
+// Searching element in an array
 void search()
 {
-    int ele;
-    printf("\nEnter the element which you want to  search : \t");
+    int ele,count=0;
+    printf("\nEnter the element to search : \t\n");
     scanf("%d", &ele);
-    
-    for (i =0; i < n - 1; i++)
+    for (i = 0; i <= n - 1; i++)
     {
-        if(a[i] == ele){
-            printf("%d",i);
-        }
+	if (a[i] == ele)
+	{
+	    printf("%d", i+1);
+	    count++;
+	}
     }
-    
+    if(count != 1)
+    {
+	printf("\nElement not found in the Array.");
+    }
 }
